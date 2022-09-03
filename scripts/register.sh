bb35ad8c2d3def9b2da1632f49d0821e2ecd0005
Jaeyoung Lim <jalim@ethz.ch>
2022-09-03 12:41:33 +0200

    Output transformation file

--- scripts/register.sh
+++ scripts/register.sh
@@ -13,4 +13,5 @@ colmap model_aligner \
     --ref_images_path $DATASET_PATH/camera.txt \
     --alignment_type enu \
     --robust_alignment 1 \
-    --robust_alignment_max_error 3
+    --robust_alignment_max_error 3 \
+    --transform_path $DATASET_PATH/transform.txt
