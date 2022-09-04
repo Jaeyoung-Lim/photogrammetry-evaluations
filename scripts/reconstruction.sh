dd132cba2731cd5f4425e249c88d2db786e9f226
Jaeyoung Lim <jalim@ethz.ch>
2022-09-04 22:12:54 +0200

    Output aligned sparse model to a different path

--- scripts/reconstruction.sh
+++ scripts/reconstruction.sh
@@ -12,26 +12,29 @@ colmap feature_extractor \
 colmap exhaustive_matcher \
    --database_path $DATASET_PATH/database.db
 
-mkdir $DATASET_PATH/sparse
+mkdir -p $DATASET_PATH/sparse
 
 colmap mapper \
     --database_path $DATASET_PATH/database.db \
     --image_path $DATASET_PATH/images \
     --output_path $DATASET_PATH/sparse
 
+mkdir -p $DATASET_PATH/sparse/1
+
 colmap model_aligner \
     --input_path $DATASET_PATH/sparse/0/ \
-    --output_path $DATASET_PATH/sparse/0/ \
+    --output_path $DATASET_PATH/sparse/1 \
     --ref_images_path $DATASET_PATH/camera.txt \
     --alignment_type enu \
     --robust_alignment 1 \
-    --robust_alignment_max_error 3
+    --robust_alignment_max_error 3 \
+    --transform_path $DATASET_PATH/transform.txt
 
 mkdir $DATASET_PATH/dense
 
 colmap image_undistorter \
     --image_path $DATASET_PATH/images \
-    --input_path $DATASET_PATH/sparse/0 \
+    --input_path $DATASET_PATH/sparse/1 \
     --output_path $DATASET_PATH/dense \
     --output_type COLMAP \
     --max_image_size 2000
