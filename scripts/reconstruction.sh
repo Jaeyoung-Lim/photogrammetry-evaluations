b77da0c12ba7d855fe11df26ffdf62e434d8da44
Jaeyoung Lim <jalim@ethz.ch>
2022-09-08 17:16:28 +0200

    Improve printouts for timing information

--- scripts/reconstruction.sh
+++ scripts/reconstruction.sh
@@ -15,7 +15,7 @@ colmap feature_extractor \
 
 echo "Elapsed Times for colmap"
 current_time=$(date +%s.%3N)
-echo "  - Feature extractor:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
+echo "  - Feature extractor [seconds]:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
 
 process_start_time=$(date +%s.%3N)
 
@@ -23,7 +23,7 @@ colmap exhaustive_matcher \
    --database_path $DATASET_PATH/database.db
 
 current_time=$(date +%s.%3N)
-echo "  - Exhaustive Matcher:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
+echo "  - Exhaustive Matcher [seconds]:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
 
 mkdir -p $DATASET_PATH/sparse
 
@@ -35,7 +35,7 @@ colmap mapper \
     --output_path $DATASET_PATH/sparse
 
 current_time=$(date +%s.%3N)
-echo "  - Mapper            :  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
+echo "  - Mapper        [seconds]:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
 
 mkdir -p $DATASET_PATH/sparse/1
 
@@ -58,7 +58,7 @@ colmap model_aligner \
     --transform_path $DATASET_PATH/transform.txt
 
 current_time=$(date +%s.%3N)
-echo "  - Model Aligner  elapsed time: " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
+echo "  - Model Aligner  elapsed time [seconds]: " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
 
 echo "============================================"
 echo "COLMAP Image_undistorter"
@@ -77,7 +77,7 @@ colmap image_undistorter \
     --max_image_size 2000
 
 current_time=$(date +%s.%3N)
-echo "  - Image Undistorter:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
+echo "  - Image Undistorter [seconds]:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
 
 process_start_time=$(date +%s.%3N)
 
@@ -87,7 +87,7 @@ colmap patch_match_stereo \
     --PatchMatchStereo.geom_consistency true
 
 current_time=$(date +%s.%3N)
-echo "  - Patchmatch Stereo:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
+echo "  - Patchmatch Stereo [seconds]:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
 
 process_start_time=$(date +%s.%3N)
 
@@ -98,7 +98,7 @@ colmap stereo_fusion \
     --output_path $DATASET_PATH/dense/fused.ply
 
 current_time=$(date +%s.%3N)
-echo "  - Stereo Fusion:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
+echo "  - Stereo Fusion [seconds]:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
 
 
 process_start_time=$(date +%s.%3N)
@@ -108,7 +108,7 @@ colmap poisson_mesher \
     --output_path $DATASET_PATH/dense/meshed-poisson.ply
 
 current_time=$(date +%s.%3N)
-echo "  - Poission Mesher:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
+echo "  - Poission Mesher [seconds]:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
 
 process_start_time=$(date +%s.%3N)
 
@@ -117,7 +117,7 @@ colmap delaunay_mesher \
     --output_path $DATASET_PATH/dense/meshed-delaunay.ply
 
 current_time=$(date +%s.%3N)
-echo "  - Delaunay Mesher:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
+echo "  - Delaunay Mesher [seconds]:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
 
 current_time=$(date +%s.%3N)
-echo "  - Total elapsed Time:  " $(echo "scale=3; $current_time - $process_start_time" | bc) >> $OUTPUT_PATH/timing.txt
+echo "  - Total elapsed Time [seconds]:  " $(echo "scale=3; $current_time - $start_time" | bc) >> $OUTPUT_PATH/timing.txt
