76a388a7a42dcd366c4dca165cab4adf18acdcef
Jaeyoung Lim <jalim@ethz.ch>
2022-09-10 22:40:07 +0200

    Visualize multiple benchmarks

--- Tools/visualize_evaluation.py
+++ Tools/visualize_evaluation.py
@@ -1,33 +1,38 @@
 import evaluate_map
+
+import yaml
 import argparse
 import matplotlib.pyplot as plt
 
-def plot_evaluation(fig, name, path, threshold):
+def plot_evaluation(ax, name, path, threshold):
     num_images, completeness, precision = evaluate_map.model_evaluation(path, threshold)
 
-    axis1 = fig.add_subplot(2, 1, 1)
-    axis1.set_xlabel('Number of Images')
-    axis1.set_ylabel('Completeness')
-    axis1.plot(num_images, completeness, '-o', label=name)
-    axis1.set_xlim([0.0, 50.0])
-    axis1.grid(True)
-    axis1.legend()
+    ax[0].set_xlabel('Number of Images')
+    ax[0].set_ylabel('Precision')
+    ax[0].plot(num_images, precision, '-o', label=name)
+    ax[0].set_xlim([0.0, 50.0])
+    ax[0].grid(True)
+    ax[0].legend()
 
-    axis2 = fig.add_subplot(2, 1, 2)
-    axis2.set_xlabel('Number of Images')
-    axis2.set_ylabel('Precision')
-    axis2.plot(num_images, precision, '-o', label=name)
-    axis2.set_xlim([0.0, 50.0])
-    axis2.grid(True)
-    axis2.legend()
+    ax[1].set_xlabel('Number of Images')
+    ax[1].set_ylabel('Completeness')
+    ax[1].plot(num_images, completeness, '-o', label=name)
+    ax[1].set_xlim([0.0, 50.0])
+    ax[1].grid(True)
+    ax[1].legend()
 
 def accumulate_evaluation(path, threshold):
-    figure1 = plt.figure("Map Data")
-
-    #TODO: Iterate over multiple datasets
-    plot_evaluation(figure1, 'Ours', path, threshold)
-    plt.tight_layout()
-    plt.show()
+    figure1, ax = plt.subplots(2, 1)
+    with open(path) as file:
+        list = yaml.load(file, Loader=yaml.FullLoader)
+        print(list)
+        for key, value in list.items():
+            data_path = value['path']
+            data_label = value['name']
+            print(data_path)
+            plot_evaluation(ax, data_label, data_path, threshold)
+        plt.tight_layout()
+        plt.show()
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('path', metavar='p', type=str, help='path to datafile')
